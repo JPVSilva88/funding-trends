0990128f2703cbab8d8d7cd58a2f0e4846a153d6
JPVSilva88 <joao.v.silva88@gmail.com>
2018-07-25 12:24:36 +0900

    Added Google Analytics

--- src/index.js
+++ src/index.js
@@ -5,8 +5,12 @@ import App from './App';
 import { unregister } from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import configureStore from './stores';
+import ReactGA from 'react-ga';
 
 const store = configureStore();
+
+ReactGA.initialize('UA-122837172-1');
+ReactGA.pageview("home", null, null);
 
 ReactDOM.render(
     <Provider store={store}>
